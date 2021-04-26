d0e07ced65c06694ebc468aa5ad456382adfb0c6
edwardoughton <edward.oughton@gmail.com>
2021-04-26 21:20:03 +0100

    Add countries input

---
+++ scripts/countries.py
@@ -0,0 +1,13 @@
+"""
+Country Assessment list
+
+"""
+
+country_list = [
+    {
+        'country_name': 'Gambia (the)', 'iso3': 'GMB', 'iso2': 'GM',
+        'regional_level': 2, 'region': 'Africa',
+        'coverage_4G': 70, 'pop_density_km2': 100, 'settlement_size': 200,
+        'subs_growth': 5, 'cluster': 1, 'smartphone_growth': 5,
+    }
+]
