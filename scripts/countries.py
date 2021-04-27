774bb03c5213b746127d3212b201a54728ba562f
edwardoughton <edward.oughton@gmail.com>
2021-04-27 21:01:21 +0100

    Edit info

--- scripts/countries.py
+++ scripts/countries.py
@@ -8,6 +8,9 @@ country_list = [
         'country_name': 'Gambia (the)', 'iso3': 'GMB', 'iso2': 'GM',
         'regional_level': 2, 'region': 'Africa',
         'coverage_4G': 70, 'pop_density_km2': 100, 'settlement_size': 200,
-        'subs_growth': 5, 'cluster': 1, 'smartphone_growth': 5,
+        'subs_growth_low': 2, 'subs_growth_baseline': 3, 'subs_growth_high': 4,
+        'sp_growth_low_urban': 5, 'sp_growth_baseline_urban': 5, 'sp_growth_high_urban': 5,
+        'sp_growth_low_rural': 5, 'sp_growth_baseline_rural': 5, 'sp_growth_high_rural': 5,
+        # 'phone_ownership_male': 0.54, 'phone_ownership_female': 0.46,
     }
 ]
